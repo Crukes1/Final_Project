03862f35e7a70a87f44b02deed6fff51871df504
Meltam21 <meltam@iu.edu>
2025-04-29 23:54:19 -0400

    added gallery and contact

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 function showAlert() {
   alert("Mothman was reportedly sighted just before major disasters. Coincidence?");
 }
+
+
+function openLightbox(imgElement) {
+  const lightbox = document.getElementById('lightbox');
+  const lightboxImg = document.getElementById('lightbox-img');
+  lightbox.style.display = 'flex';
+  lightboxImg.src = imgElement.src;
+  lightboxImg.alt = imgElement.alt;
+}
+
+function closeLightbox() {
+  document.getElementById('lightbox').style.display = 'none';
+}
